b3a6150ffc0a71845380f6c92ab43ae15f29475f
Laura Pajaro <lauratecno30@google.com>
2025-03-13 16:17:58 -0300

    Fetch data

--- src/pages/Proyecciones/ProyeccionesSlice.js
+++ src/pages/Proyecciones/ProyeccionesSlice.js
@@ -1,8 +1,60 @@
 import { createSlice } from '../../redux/createSlice.js';
 import { MathFunction, MathFunctionsTemplates } from '../../mathFunctions/index.js';
-
+import axios from 'axios';
 const { newConstant, newLine } = MathFunctionsTemplates;
+const cacResponse = (await axios.get('https://prestamos.ikiwi.net.ar/api/cacs'))?.data;
+const uvaResponse = (await axios.get("https://prestamos.ikiwi.net.ar/api/v1/engine/uva/valores/"))?.data;
+const uviResponse = (await axios.get("https://prestamos.ikiwi.net.ar/api/v1/engine/uvi/valores/"))?.data;
+// const dolaritoResponse = ''
+// await fetch("https://www.dolarito.ar/api/frontend/history", {
+//   "headers": {
+//     "accept": "application/json, text/plain, */*",
+//     "auth-client": "0022200edebd6eaee37427532323d88b",
+//     "sec-ch-ua": "\"Google Chrome\";v=\"123\", \"Not:A-Brand\";v=\"8\", \"Chromium\";v=\"123\"",
+//     "sec-ch-ua-mobile": "?0",
+//     "sec-ch-ua-platform": "\"Windows\"",
+//     "Referer": "https://www.dolarito.ar/cotizaciones-historicas/informal",
+//     "Referrer-Policy": "strict-origin-when-cross-origin"
+//   },
+//   "body": null,
+//   "method": "GET"
+// })
+async function fetchData() {
+    try {
+        const response = await fetch("/api/dolarito-history", {
+            "headers": {
+                "accept": "application/json, text/plain, */*",
+                "auth-client": "0022200edebd6eaee37427532323d88b",
+                "sec-ch-ua": "\"Google Chrome\";v=\"123\", \"Not:A-Brand\";v=\"8\", \"Chromium\";v=\"123\"",
+                "sec-ch-ua-mobile": "?0",
+                "sec-ch-ua-platform": "\"Windows\"",
+                "Referer": "https://www.dolarito.ar/cotizaciones-historicas/informal",
+                "Referrer-Policy": "strict-origin-when-cross-origin"
+            },
+            "method": "GET"
+        });
 
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error("Error al hacer la petición:", error);
+    }
+}
+const dolaritoResponse = fetchData();
+const today = new Date(Date.UTC(
+    new Date().getUTCFullYear(),
+    new Date().getUTCMonth(),
+    new Date().getUTCDate()
+));
+const year = today.getUTCFullYear();
+const month = String(today.getUTCMonth() + 1).padStart(2, '0'); // Mes en formato 'MM'
+const day = String(today.getUTCDate()).padStart(2, '0'); // Día en formato 'DD'
+const dataKey = `${day}-${month}-${String(year).slice(2)}`
+console.log({ cacResponse, uvaResponse, uviResponse, dolaritoResponse: dolaritoResponse[dataKey], year,month,day })
 const proyeccionesSlice = createSlice({
     name: 'parameters',
     initialState: {
@@ -12,15 +64,16 @@ const proyeccionesSlice = createSlice({
         data: {
             duration: 36,
             cashflows: [
-                { name: 'Terreno', serialized: newLine({ a: 100, b: 200}, { minX: 6, maxX: 16 }).serialize(), unitId: 2, factB: 0.5, IIBB: 0.01, iva: 0.21, color: '#CE4893' },
-                { name: 'Construcción', serialized: newConstant({ value: -1400}, { minX: 0, maxX: 23 }).serialize(), unitId: 2, factB: 0.5, IIBB: 0.01, iva: 0.21, color: '#5E4DB2' },
+                { name: 'Terreno', serialized: newLine({ a: 100, b: 200 }, { minX: 6, maxX: 16 }).serialize(), unitId: 2, factB: 0.5, IIBB: 0.01, iva: 0.21, color: '#CE4893' },
+                { name: 'Construcción', serialized: newConstant({ value: -1400 }, { minX: 0, maxX: 23 }).serialize(), unitId: 2, factB: 0.5, IIBB: 0.01, iva: 0.21, color: '#5E4DB2' },
                 { name: 'Comercial', serialized: newConstant({ value: -600 }, { minX: 0, maxX: 23 }).serialize(), unitId: 2, factB: 0.5, IIBB: 0.01, iva: 0.21, color: '#FFDE59' },
                 { name: 'Venta', serialized: newConstant({ value: 1000 }, { minX: 0, maxX: 30 }).serialize(), unitId: 2, factB: 0.5, IIBB: 0.01, iva: 0.21, color: '#BBEE53' },
             ],
             indexes: [
-                { id: 1, name: 'CAC', label: 'CAC', serialized: newConstant({ value: 1000 }, { minX: 0, maxX: 23 }).serialize(), unitId: 'CAC',color: '#5E4DB2' },
-                { id: 2, name: 'Dolar Blue', label: 'U$', serialized: newConstant({ value: 1000 }, { minX: 0, maxX: 23 }).serialize(), unitId: 'U$',color: '#BBEE53' },
-                { id: 3, name: 'Precio M2 Homogeneizado', label: 'M2 Hom.', serialized: newConstant({ value: 4000 }, { minX: 0, maxX: 23 }).serialize(), unitId: 'm2',color: '#FFDE59' },
+                { id: 1, name: 'CAC', label: 'CAC', serialized: newConstant({ value: cacResponse[cacResponse?.length - 1]?.general || 1000 }, { minX: 0, maxX: 36 }).serialize(), unitId: 'CAC', color: '#5E4DB2' },
+                { id: 2, name: 'Dolar Blue', label: 'U$', serialized: newConstant({ value: dolaritoResponse[dataKey]?.informal?.venta || 1000 }, { minX: 0, maxX: 36 }).serialize(), unitId: 'U$', color: '#BBEE53' },
+                { id: 3, name: 'Precio M2 Homogeneizado', label: 'M2 Hom.', serialized: newConstant({ value: 4000 }, { minX: 0, maxX: 36 }).serialize(), unitId: 'm2', color: '#FFDE59' },
+                { id: 3, name: 'UVA', label: 'UVA', serialized: newConstant({ value: uvaResponse[0]?.valor }, { minX: 0, maxX: 36 }).serialize(), unitId: 'm2', color: '#FFDE59' },
             ],
         },
     },
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api/dolarito-history": {
+        target: "https://www.dolarito.ar",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/dolarito-history/, "/api/frontend/history"),
+      },
+    },
+  },
 })
